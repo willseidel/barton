1a6d71862fc690b9919256c0bf2c71d1c56f0afa
willseidel <will.seidel@gmail.com>
2016-07-10 22:16:43 -0700

    fleshed out logic
    A bit ugly, but the script now creates a grid based on a user-specified
    order and density per order. This grid is searched and the best reward
    is found. From there a new grid is made based on multipliers of the
    best parameters and we search that grid for the new best parameters.

--- dmbgen.py
+++ dmbgen.py
@@ -33,7 +33,7 @@ def evalParamSet(env,nRuns,divs,nAcs,params,render):
 			#######^^^^^^##############
 
 			observation, reward, done, info  = env.step(action)
-			if render:
+			if (render and j==0):
 				env.render()
 			if done:
 				break
@@ -42,10 +42,7 @@ def evalParamSet(env,nRuns,divs,nAcs,params,render):
 
 
 		rewardRecord += [reward_cum]
-		print "run#: ",j
-		print "reward: ", reward_cum
-		print "params: ", params
-		print "\n"
+
 
 
 	#meanReward =  np.mean(rewardRecord)
@@ -67,20 +64,26 @@ def paramSweep(paramVals,nSet):
 
 
 #the environment
-env 			= gym.make('MountainCar-v0')
+environmentName = 'MountainCar-v0'
+env 			= gym.make(environmentName)
 
 #inputs
-nSteps 			= 5000
-nRunsAvg 		= 1
-
+nSteps 				= 500
+nRunsAvg 			= 1
+nRunsMax			= 100
+paramGridOrder		= 5 #search grid will go up/down by this many orders of magnitude
+paramGridDensity 	= 5 	#number of elements in grid per order (ie order 1e0 w/density=5)...
+							#includes 2 4 6 8 10
 
 #initializing other variables
-render 			= False
-actionOptions 	= []
-params 			= [] #parameters we will optimize
-dotSpaceMax		= 1 #this is the magnitude that will govern the binning of actions based...
-divs			= []	
-paramGridOrder	= 6 #search grid will go up/down by this many orders of magnitude
+actionOptions 		= []
+params 				= [] #parameters we will optimize
+dotSpaceMax			= 1 #this is the magnitude that will govern the binning of actions based...
+divs				= []	
+rewardMeanRecord 	= []
+paramGridValues 	= []
+bestReward 			= -10e10
+nRunsCum 			= 0 #total number of runs
 
 #this section just finds the range of discrete actions
 for i in range(100):
@@ -103,19 +106,46 @@ else: #
 		divs += [dotSpaceMax - (i-1)*((2*dotSpaceMax)/(nAcs-2))]
 
 ##########SCRATCH FOR LOOPING THROUGH GRID
-rewardMeanRecord = []
-paramGridValues = []
-
 for i in range(0,paramGridOrder):
-	paramGridValues += [10**i]
-	paramGridValues += [-10**i]
+	for k in range(0,paramGridDensity):
+		paramGridValues += [((k+1)*(10/paramGridDensity))*(10**i)]
+		paramGridValues += [-((k+1)*(10/paramGridDensity))*(10**i)]
 
 paramSet = paramSweep(paramGridValues,len(params))
+bestParams = paramSet[1]
+while nRunsCum<nRunsMax:
 
-for i in range(0,len(paramSet)):
-	rewardMeanRecord += [evalParamSet(env,nRunsAvg,divs,nAcs,paramSet[i],render)]
+	paramGridValues = [] #resetting grid values from last loop
+	rewardMeanRecord = [] #resetting record of rewards from last loop
 
-bestParams = paramSet[rewardMeanRecord.index(max(rewardMeanRecord))]
+	for i in range(0,len(paramSet)):
+		rewardMeanRecord += [evalParamSet(env,nRunsAvg,divs,nAcs,paramSet[i],render=False)]
+		nRunsCum +=nRunsAvg
+		print "total runs: ", nRunsCum
+		print "mean reward of last set: ", rewardMeanRecord[-1]
 
+	numberOfHits = sum(rewardMeanRecord>min(rewardMeanRecord))
+
+	if max(rewardMeanRecord)>bestReward and numberOfHits>0:
+		bestReward = max(rewardMeanRecord)
+		bestParams = paramSet[rewardMeanRecord.index(max(rewardMeanRecord))]
+		print "best average reward:", bestReward
+		print "best params: ", bestParams
+		rewardMeanRecord += [evalParamSet(env,nRunsAvg,divs,nAcs,bestParams,render=True)] #run to show off
+
+		for k in range(1,paramGridDensity): #setting new tighter grid around 'best' values
+			for l in range(0,len(bestParams)):
+				paramGridValues += [(1+(k/10.0))*bestParams[l]]
+				paramGridValues += [-(1+(k/10.0))*bestParams[l]]
+
+		paramSet = paramSweep(paramGridValues,len(params)) #new paramSet
+
+
+rewardMeanRecord = [evalParamSet(env,nRunsAvg,divs,nAcs,bestParams,render=True)] #run to show off
+print "************"
+print "best average reward:", bestReward
+print "best params: ", bestParams
+
+env.render(close=True)
 
 
